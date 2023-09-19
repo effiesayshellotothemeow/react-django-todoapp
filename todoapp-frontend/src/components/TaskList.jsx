f47086544d29df0a2a7a9a641d2d91b52bfc9708
effiesayhitothemeow <effiefiefe@gmail.com>
2023-09-20 06:34:28 +0800

    update

--- todoapp-frontend/src/components/TaskList.jsx
+++ todoapp-frontend/src/components/TaskList.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+// Component
+import TaskItem from "./TaskItem";
+// Styles
+import styles from "./TaskList.Module.css";
 
-function TaskList() {
-  return (
-    <div>
-        Task List
-    </div>
-  )
-}
+// Create a list of tasks based on the data provided in the tasks prop
+const TaskList = ({ tasks }) => {
+	return (
+		// Render and loop
+		<ul className={styles.tasks}>
+			{/* sort by id (timestamp) */}
+			{tasks
+				.sort((a, b) => b.id - a.id)
+				.map((task) => (
+					<TaskItem key={task.id} task={task} />
+				))}
+		</ul>
+	);
+};
 
-export default TaskList
+export default TaskList;
