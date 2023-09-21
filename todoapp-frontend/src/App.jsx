27ff139213e81ec84c080352fd4376f731dc1a25
effiesayhitothemeow <effiefiefe@gmail.com>
2023-09-21 22:24:27 +0800

    Edit mode updated

--- todoapp-frontend/src/App.jsx
+++ todoapp-frontend/src/App.jsx
@@ -1,72 +1,75 @@
 import { useState } from "react";
 
 //Components
-import CustomForm from "./components/CustomForms";
+import CustomForms from "./components/CustomForms";
 import EditForms from "./components/EditForms";
 import TaskList from "./components/TaskList";
 
 function App() {
-    // Create a variable to store tasks ;and a function to update it.
+	// Create a variable to store tasks ;and a function to update it.
 	const [tasks, setTasks] = useState([]);
-    const [editedTask, setEditedTask] = useState(null);
+	const [editedTask, setEditedTask] = useState(null);
+	// Check whether or not in editing mode
+	const [isEditing, setIsEditing] = useState(false);
 
-    // Add task function
-    const addTask = (task , deleteTask) => {
-        // Update the list of tasks by adding the new task.
-        // Get current list (prevState), and add the new task
-        setTasks(prevState => [...prevState, task])
-    }
+	// Add task function
+	const addTask = (task) => {
+		// Update the list of tasks by adding the new task.
+		// Get current list (prevState), and add the new task
+		setTasks((prevState) => [...prevState, task]);
+	};
 
-    // Delete task
-    const deleteTask = (id) => {
-        setTasks(prevState => prevState.filter(
-            t => t.id != id));
-    }
+	// Delete task
+	const deleteTask = (id) => {
+		setTasks(prevState => prevState.filter(t => t.id !== id));
+	};
 
-    // Check task
-    const toggleTask = (id) => {
-        setTasks(prevState.map(t => (
-            t.id == id ? { ...t, checked: !t.checked} : t
-        )))
-    }
+	// Check task
+	const toggleTask = (id) => {
+		setTasks(
+			prevState.map((t) => (t.id == id ? { ...t, checked: !t.checked } : t))
+		);
+	};
 
-    // Update task
-    const updateTask = (task) => {
-        setTasks(prevState.map(t => (
-            t.id == task.id ? { ...t, name: task.name } : t
-        )))
-        // Collapse the edit mode
-    }
+	// Update task
+	const updateTask = (task) => {
+		setTasks(
+			prevState.map((t) => (t.id == task.id ? { ...t, name: task.name } : t))
+		);
+		// Collapse the edit mode
+	};
 
-    // Enter edit mode
-    const enterEditMode = (task) => {
-        setEditedTask(task);
-    }
+	// Enter edit mode
+	const enterEditMode = (task) => {
+		setEditedTask(task);
+		// Expand editing mode
+		setIsEditing(true);
+	};
 
 	return (
-        <div className="container">
-            <header>
-                <h1>My Task List</h1>
-            </header>
+		<div className="container">
+			<header>
+				<h1>My Task List</h1>
+			</header>
 
-            <EditForms
-                editedTask={editedTask}
-                updateTask={updateTask}
-            />            
+			{/* Check whether or not in editing mode */}
+			{isEditing && (
+				<EditForms editedTask={editedTask} updateTask={updateTask} />
+			)}
 
-            {/* CustomForm has access to addTask */}
-            <CustomForm addTask={addTask}/>
+			{/* CustomForm has access to addTask */}
+			<CustomForms addTask={addTask} />
 
-            {/* Only if there are tasks , then render the TaskList component." */}
-            {tasks && (
-            <TaskList 
-                tasks={tasks}
-                deleteTask={deleteTask}
-                toggleTask={toggleTask}
-                enterEditMode={enterEditMode}
-            />
-            )}
-        </div>
+			{/* Only if there are tasks , then render the TaskList component." */}
+			{tasks.length > 0 && (
+				<TaskList
+					tasks={tasks}
+					deleteTask={deleteTask}
+					toggleTask={toggleTask}
+					enterEditMode={enterEditMode}
+				/>
+			)};
+		</div>
 	);
 }
 
